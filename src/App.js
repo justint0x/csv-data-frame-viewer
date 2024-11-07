a13bd2cb3c7c8205164d4653f41cfeb10b97496f
justint0x <pearldevman@gmail.com>
2024-11-06 19:09:44 -0500

    add - main app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import DataFrameTable from './components/DataFrameTable';
+import FileUpload from './components/FileUpload';
+
+const pandasData = {
+  "result": {
+    "columns": ["Name", "Birthdate", "Score", "Grade"],
+    "data": [
+      ["Alice", 631152000000000000, 90, "A"],
+      ["Bob", 665452800000000000, 75, "B"],
+      ["Charlie", 699580800000000000, 85, "A"],
+      ["David", 733881600000000000, 70, "B"],
+      ["Eve", 768096000000000000, null, "A"]
+    ],
+    "data_types": ["object", "int64", "object", "category"]
+  }
+};
+
 function App() {
+  const [data, setData] = useState(null)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SpreadSheet Converter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className='main'>
+        <FileUpload setData={setData}/>
+        <DataFrameTable jsonData={data} />
+      </main>
     </div>
   );
 }
